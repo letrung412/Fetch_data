fcc7cf9775c329153cb7b8948b6e4a5051b4821e
Long Dương Văn <long2291999@outlook.com>
2021-07-05 02:46:15 +0700

    Fetch

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import Fetch from './components/fetch';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<Fetch />,document.getElementById('root')
+);
+reportWebVitals();
